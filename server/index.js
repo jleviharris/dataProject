6009b974f3aec70734e010cfceb5d8f4004eecf8
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-11-20 20:11:04 -0500

    Update for the day

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const db = mysql.createConnection({
   password: "password",
   database: "employeeSystem",
 });
-
+//////////////////////    EMPLOYEES ////////////////////////
 //POST request to create new employee
 app.post("/create", (req, res) => {
   const name = req.body.name;
@@ -20,10 +20,11 @@ app.post("/create", (req, res) => {
   const country = req.body.country;
   const position = req.body.position;
   const wage = req.body.wage;
+  const manager = req.body.manager;
 
   db.query(
-    "INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)",
-    [name, age, country, position, wage],
+    "INSERT INTO employees (name, age, country, position, wage, manager) VALUES (?,?,?,?,?,?)",
+    [name, age, country, position, wage, manager],
     (err, result) => {
       if (err) {
         console.log(err);
@@ -44,6 +45,7 @@ app.get("/employees", (req, res) => {
     }
   });
 });
+
 //GET all employees ordered by name ASC
 app.get("/employeesName", (req, res) => {
   db.query("SELECT * FROM employees ORDER By name", (err, result) => {
@@ -54,6 +56,7 @@ app.get("/employeesName", (req, res) => {
     }
   });
 });
+
 //GET all employees ordered by name DESC
 app.get("/employeesNameDesc", (req, res) => {
   db.query("SELECT * FROM employees ORDER By name DESC", (err, result) => {
@@ -64,6 +67,7 @@ app.get("/employeesNameDesc", (req, res) => {
     }
   });
 });
+
 //GET all employees ordered by age ASC
 app.get("/employeesAge", (req, res) => {
   db.query("SELECT * FROM employees ORDER By age", (err, result) => {
@@ -287,10 +291,253 @@ app.put("/updateWage", (req, res) => {
     }
   );
 });
+//PUT request to update supervisor
+app.put("/updateSupervisor", (req, res) => {
+  const id = req.body.id;
+  const manager = req.body.newManager;
+  db.query(
+    "UPDATE employees SET manager = ? WHERE id = ?",
+    [manager, id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
 // DELETE employee
 app.delete("/delete/:id", (req, res) => {
   const id = req.params.id;
   db.query("DELETE FROM employees WHERE id = ?", id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+//////////////////////     MANAGERS     ////////////////////////////
+
+//POST request to create new manager
+app.post("/createManager", (req, res) => {
+  const first_name = req.body.first_name;
+  const last_name = req.body.last_name;
+  const emp_id = req.body.emp_id;
+  const title = req.body.title;
+
+  db.query(
+    "INSERT INTO managers (first_name, last_name, emp_id, title) VALUES (?,?,?,?)",
+    [first_name, last_name, emp_id, title],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values Inserted");
+      }
+    }
+  );
+});
+
+//GET all managers
+app.get("/managers", (req, res) => {
+  db.query("SELECT * FROM managers", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+//GET all managers ordered by first_name ASC
+app.get("/managerFirstName", (req, res) => {
+  db.query("SELECT * FROM managers ORDER By first_name", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+//GET all managers ordered by first_name DESC
+app.get("/managerFirstNameDesc", (req, res) => {
+  db.query("SELECT * FROM managers ORDER By first_name DESC", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+//GET all managers ordered by last_name ASC
+app.get("/managerLastName", (req, res) => {
+  db.query("SELECT * FROM managers ORDER By last_name", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+//GET all managers ordered by last_name DESC
+app.get("/managerLastNameDesc", (req, res) => {
+  db.query("SELECT * FROM managers ORDER By last_name DESC", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+//GET all managers ordered by emp_id
+app.get("/managersID", (req, res) => {
+  db.query("SELECT * FROM managers ORDER By emp_id", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+//GET all managers ordered by emp_id DESC
+app.get("/managersIDDesc", (req, res) => {
+  db.query("SELECT * FROM managers ORDER By emp_id DESC", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+//GET all managers ordered by title
+app.get("/managersTitle", (req, res) => {
+  db.query("SELECT * FROM managers ORDER By title", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+//GET all managers ordered by title DESC
+app.get("/managersTitleDesc", (req, res) => {
+  db.query("SELECT * FROM managers ORDER By title DESC", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+//PUT request to update manager first name
+app.put("/updateManagerFirstName", (req, res) => {
+  const id = req.body.manager_id;
+  const first_name = req.body.first_name;
+  db.query(
+    "UPDATE managers SET first_name = ? WHERE manager_id = ?",
+    [first_name, id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+//PUT request to update manager last name
+app.put("/updateManagerLastName", (req, res) => {
+  const id = req.body.manager_id;
+  const last_name = req.body.last_name;
+  db.query(
+    "UPDATE managers SET last_name = ? WHERE manager_id = ?",
+    [last_name, id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+//PUT request to update manager title
+app.put("/updateManagerTitle", (req, res) => {
+  const id = req.body.manager_id;
+  const title = req.body.title;
+  db.query(
+    "UPDATE managers SET title = ? WHERE manager_id = ?",
+    [title, id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+//GET manager by first name
+app.get("/firstName/:firstName", (req, res) => {
+  const first_name = req.params.firstName;
+  db.query(
+    "SELECT * FROM managers WHERE first_name = ?",
+    first_name,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+//GET manager by last name
+app.get("/lastName/:lastName", (req, res) => {
+  const input = req.params.lastName;
+  db.query(
+    "SELECT * FROM managers WHERE last_name = ?",
+    input,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+//GET manager by ID
+app.get("/id/:id", (req, res) => {
+  const input = req.params.id;
+  db.query("SELECT * FROM managers WHERE emp_id = ?", input, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+//GET manager by title
+app.get("/title/:title", (req, res) => {
+  const input = req.params.title;
+  db.query("SELECT * FROM managers WHERE title = ?", input, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+// DELETE manager
+app.delete("/deleteManager/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("DELETE FROM manager WHERE manager_id = ?", id, (err, result) => {
     if (err) {
       console.log(err);
     } else {
